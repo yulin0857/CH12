4782370ddb91254e2be9d96bd1e64575a4320e37
Yulin <hboy15913@yahoo.com>
2021-06-28 11:16:34 +0800

    12-22

--- Game.kt
+++ Game.kt
@@ -13,53 +13,25 @@ fun main() {
     val player=Player()
     player.castFireball(5)
 
-    val karma = (pow(random(),(110-healthPoints)/100.0)*20).toInt()
+    //aura
+    val auraColor = player.auraColor()
+
+    //Player status
+    printPlayerStatus(player)
 
     //aura
-    val auraColor = auraColor(isBlessed, healthPoints, isImmortal, karma)
-
-    val healthStatus = fotmatHealthStatus(healthPoints, isBlessed)
-    printPlayerStatus(healthPoints, auraColor, isBlessed, player.name, healthStatus)
-
-
-
+    player.auraColor()
 }
 
-private fun printPlayerStatus(
-    healthPoints: Int,
-    auraColor: String,
-    isBlessed: Boolean,
-    name: String,
-    healthStatus: String
-) {
-    val statusFormatString =
-        "(健康指數: $healthPoints)(光環: $auraColor)(運勢: ${if (isBlessed) "走運" else "很背"}) -> $name $healthStatus"
-    println(statusFormatString)
+private fun printPlayerStatus(player: Player)
+{
+    println("(光環: ${player.auraColor()})"+"(運勢: ${if (player.isBlessed) "走運" else "很背"})")
+            println("${player.name} ${player.formatHealthStatus()}")
 }
 
-private fun auraColor(
-    isBlessed: Boolean,
-    healthPoints: Int,
-    isImmortal: Boolean,
-    karma: Int
-): String {
-    val auraVisible = isBlessed && healthPoints > 50 || isImmortal
-    val auraColor = auraColor(karma)
-    return auraColor
-}
 
-private fun auraColor(karma: Int): String {
-    val auraColor = when (karma) {
-        in 0..5 -> "紅色"
-        in 6..10 -> "橘色"
-        in 11..15 -> "紫色"
-        in 16..20 -> "綠色"
-        else -> "無光環"
-    }
-    return auraColor
-}
 
-private fun fotmatHealthStatus(healthPoints: Int, isBlessed: Boolean): String {
+/*private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean): String {
     val healthStatus = when (healthPoints) {
         100 -> "健康狀態極佳"
         in 90..99 -> "有一些小擦傷"
@@ -74,7 +46,7 @@ private fun fotmatHealthStatus(healthPoints: Int, isBlessed: Boolean): String {
     }
     return healthStatus
 }
-/*private fun castFireball(numFireball: Int = 2): Int{
+private fun castFireball(numFireball: Int = 2): Int{
     println("已喝下$numFireball 杯Fireball")
     return numFireball
 }*/
